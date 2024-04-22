fab2369c7ad9bf2fee38e6e66e39b0de575f93a1
agil <mughni@mhs.usk.ac.id>
2024-04-22 11:41:35 +0700

    feat: change style script

--- js/barChart.js
+++ js/barChart.js
@@ -1,29 +1,28 @@
-var svg = d3.select("svg"),
+var svg = d3.select("#bar-chart"),
     margin = { top: 100, right: 100, bottom: 100, left: 150 },
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
 
 svg.append("text")
-   .attr("transform", "translate(100,0)")
-   .attr("x", 50)
-   .attr("y", 50)
-   .attr("font-size", "24px")
-   .text("Top 10 Negara dengan Rata-rata Polutan NO Terbanyak");
+    .attr("transform", "translate(100,0)")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("font-size", "24px")
 
 var xScale = d3.scaleBand().range([0, width]).padding(0.4),
     yScale = d3.scaleLinear().range([height, 0]);
 
 var g = svg.append("g")
-           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Baca data dari file CSV
-d3.dsv(';', "./data/airQuality_out.csv").then(function(data) {
+d3.dsv(';', "./data/airQuality_out.csv").then(function (data) {
     // Kelompokkan data berdasarkan kode negara (Country Label)
     var groupedData = d3.group(data, d => d["Country Label"]); // Menggunakan "Country Label" sebagai kelompok
 
     // Hitung total nilai polutan NO dan jumlah kemunculan NO untuk setiap negara
     var noData = [];
-    groupedData.forEach(function(countryData, countryLabel) {
+    groupedData.forEach(function (countryData, countryLabel) {
         var totalNO = d3.sum(countryData, d => d.Pollutant === "NO" ? +d.Value_standard : 0);
         var countNO = countryData.filter(d => d.Pollutant === "NO").length;
         var averageNO = countNO > 0 ? totalNO / countNO : 0; // Rata-rata polutan NO
@@ -42,33 +41,39 @@ d3.dsv(';', "./data/airQuality_out.csv").then(function(data) {
 
     // Gambar sumbu x dan sumbu y
     g.append("g")
-     .attr("transform", "translate(0," + height + ")")
-     .call(d3.axisBottom(xScale))
-     .selectAll("text")
-     .attr("transform", "rotate(-45)")
-     .style("text-anchor", "end")
-     .attr("dx", "-.8em")
-     .attr("dy", ".15em");
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale))
+        .selectAll("text")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em");
 
     g.append("g")
-     .call(d3.axisLeft(yScale).tickFormat(d => d + " µg/m³").ticks(10)) // mengatur langkah tick dan format
-     .append("text")
-     .attr("y", 6)
-     .attr("dy", "-2em")
-     .attr("dx","3em")
-     .attr("text-anchor", "end")
-     .attr("stroke", "black")
-     .text("Average NO Value (µg/m³)");
+        .call(d3.axisLeft(yScale).tickFormat(d => d + " µg/m³").ticks(10)) // mengatur langkah tick dan format
+        .append("text")
+        .attr("y", 6)
+        .attr("dy", "-2em")
+        .attr("dx", "3em")
+        .attr("text-anchor", "end")
+        .attr("fill", "#0E0B16")
+        .attr("font-weight", 900)
+        .text("Average NO Value (µg/m³)");
+
+    // Merubah warna teks pada sumbu y menjadi lightgray
+    g.selectAll(".tick text")
+        .style("fill", "#0E0B16");
+
 
     // Gambar grafik batang
     g.selectAll(".bar")
-     .data(top10)
-     .enter().append("rect")
-     .attr("class", "bar")
-     .attr("x", function(d) { return xScale(d["Country Label"]); })
-     .attr("y", function(d) { return yScale(d["Average NO"]); })
-     .attr("width", xScale.bandwidth())
-     .attr("height", function(d) { return height - yScale(d["Average NO"]); });
-}).catch(function(error) {
+        .data(top10)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return xScale(d["Country Label"]); })
+        .attr("y", function (d) { return yScale(d["Average NO"]); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function (d) { return height - yScale(d["Average NO"]); });
+}).catch(function (error) {
     console.log(error);
 });
